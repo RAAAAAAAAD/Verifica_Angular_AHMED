57b708d8c0d41185df118180117244e82433605b
RadIbn Ahmed <radahmed62@gmail.com>
2025-02-26 12:36:30 +0000

    Punto 3

--- Ahmed_Radibn_filaB/src/app/app.component.ts
+++ Ahmed_Radibn_filaB/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { QuartoComponent } from './quarto/quarto.component';
+import { TerzoComponent } from './terzo/terzo.component';
+import { SecondoComponent } from './secondo/secondo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, QuartoComponent, TerzoComponent, SecondoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
